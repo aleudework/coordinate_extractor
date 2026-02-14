1bf4a1cbaad46f192e14197ddbf472fb56a7fd9c
Alexander Holten Udengaard <alhu@Mac-Studio-DAB-Lejerbo.local>
2026-02-14 18:05:47 +0100

    Adjusted coordinates

--- afdeling_koordinator.py
+++ afdeling_koordinator.py
@@ -3,23 +3,33 @@ from goldcode.utils.db_downloader import DBDownloader
 import pandas as pd
 import requests
 
-
 def get_lat_long(row):
     try:
+        # -------- Første forsøg: adresse + postnr --------
         url = "https://api.dataforsyningen.dk/adresser"
         params = {
             "vejnavn": row["adresse"],
             "postnr": str(row["post"])
         }
 
-        
         response = requests.get(url, params=params)
         data = response.json()
 
+        print('OK')
+
         if len(data) > 0:
             lon, lat = data[0]["adgangsadresse"]["adgangspunkt"]["koordinater"]
-            print(lon, ' - ', lat)
             return lat, lon
+
+        # -------- Fallback: kun postnummer --------
+        url_post = "https://api.dataforsyningen.dk/postnumre"
+        response_post = requests.get(url_post, params={"nr": str(row["post"])})
+        data_post = response_post.json()
+
+        if "visueltcenter" in data_post:
+            lon, lat = data_post["visueltcenter"]["koordinater"]
+            return lat, lon
+
     except:
         return None, None
 
@@ -64,22 +74,27 @@ def main(output_file, afdelings_id='./data/afdelings_id.xlsx'):
 
         df_merged = pd.merge(df, df_afd_id, "left", left_on=['sel', 'afd'], right_on=['Selskabsnr', 'Afdelingsnr'])
 
-        print(df_merged.info())
-
         df_merged["post"] = df_merged["postby"].str.split().str[0]
 
+        df_merged["Importeret"] = '2026-02-14'
+
         # Requester for lat og long
+
+        print(df_merged.info())
+
+        print(df_merged.head(5))
+
         
-        df[["latitude", "longitude"]] = df.apply(
+        df_merged[["latitude", "longitude"]] = df_merged.apply(
             lambda row: pd.Series(get_lat_long(row)),
             axis=1
         )
 
-        df.to_excel(output_file, index=False)
+        df_merged.to_excel(output_file, index=False)
 
     except Exception as e:
         print(e)
 
 
-def __init__():
+if __name__ == '__main__':
     main(output_file = './Afdelingskoordinater.xlsx')
